f5f376844e3c16a58ddb7c9584856a7dc24ae20c
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2023-06-26 22:26:55 +1000

    enhance tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import ThemeModeToggle from './components/ThemeMode/ThemeModeToggle'
 
 function App() {
   return (
-    <div className='flex flex-col items-center min-h-screen gap-10 p-24'>
+    <div className='flex min-h-screen flex-col items-center gap-10 p-24'>
       <ThemeModeToggle title='Dark Mode' />
       <div style={{ textAlign: 'center' }}>
-        <h1 className='text-3xl font-bold text-green'>GeeksforGeeks</h1>
+        <h1 className='text-green text-3xl font-bold'>GeeksforGeeks</h1>
         <h3 className='text-2xl'>Adding Dark Mode in ReactJS using Tailwind CSS</h3>
       </div>
       <center>
-        <div className='w-56 overflow-hidden bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
+        <div className='w-56 overflow-hidden rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800'>
           <img
             className='rounded-t-lg'
             src='https://media.geeksforgeeks.org/wp-content/uploads/20220221132017/download.png'
