0628575068cfbc1dccaf4789613f28bdd4533020
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2023-07-23 22:00:54 +1000

    add debug for mocked endpoints

--- src/components/api/fetch/stub.ts
+++ src/components/api/fetch/stub.ts
@@ -1,4 +1,4 @@
-import { AxiosInstance } from 'axios'
+import { AxiosInstance, AxiosRequestConfig } from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import { routes } from '../ApiRouteSchema'
 import { createItemAction, getScreen, getScreenList } from '../db/indexApi'
@@ -6,13 +6,16 @@ import { createItemAction, getScreen, getScreenList } from '../db/indexApi'
 // Stub out the API calls using axios-mock-adapter for indexAPI to store data in the browser's IndexedDB
 // The stubbed API calls can later be replaced with real API calls to a backend store
 export const generateStub = (axiosInstance: AxiosInstance) => {
-  const timeout = 1000
+  const mock = new MockAdapter(axiosInstance, { onNoMatch: 'passthrough', delayResponse: 1000 })
 
-  const mock = new MockAdapter(axiosInstance, { onNoMatch: 'passthrough' })
+  const debug = (config: AxiosRequestConfig, response: unknown) => {
+    console.debug(`${config.method}: ${config.url}`, '\nRequest:', config, '\nResponse Data:', response)
+  }
 
   mock.onGet(`${routes.baseUrl}${routes.root}/${routes.screens.path}`).reply((config) =>
     getScreenList(config.url || '').then((payload) => {
-      setTimeout(() => {}, timeout)
+      debug(config, payload)
+
       return [
         200,
         {
@@ -25,7 +28,7 @@ export const generateStub = (axiosInstance: AxiosInstance) => {
 
   mock.onGet(`${routes.baseUrl}${routes.root}/${routes.screens.path}/${routes.screens.key}`).reply((config) =>
     getScreen(config.params || {}).then((payload) => {
-      setTimeout(() => {}, timeout)
+      debug(config, payload)
 
       return [
         200,
@@ -41,7 +44,7 @@ export const generateStub = (axiosInstance: AxiosInstance) => {
     .onPost(`${routes.baseUrl}${routes.root}/${routes.screens.path}/${routes.screens.actions.create}`)
     .reply((config) =>
       createItemAction(config.data || {}).then((payload) => {
-        setTimeout(() => {}, timeout)
+        debug(config, payload)
 
         return [
           200,
