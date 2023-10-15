f52814615a4f4a063be46f98e3e9139d6f52d853
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2023-10-15 20:47:32 +1100

    adjust borders in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,11 +3,11 @@ import ThemeModeToggle from '../theme/ThemeModeToggle'
 export default function Header() {
   return (
     <>
-      <div className='header grid place-content-center pt-2 text-xl'>Screen Geometry</div>
-      <div className='sidebar sticky top-0 z-10 rounded-b-xl'>
+      <div className='header grid place-content-center pt-2 text-2xl'>Screen Geometry</div>
+      <div className='sidebar sticky top-0 z-10 rounded-b-xl px-2'>
         <header>
-          <div className='flex items-center justify-between px-3 py-2 lg:px-4'>
-            <ul className='flex list-none justify-center gap-4'>
+          <div className='flex items-center justify-between py-4 lg:px-4'>
+            <ul className='flex list-none justify-center gap-6'>
               <li>
                 <a href='/'>Home</a>
               </li>
