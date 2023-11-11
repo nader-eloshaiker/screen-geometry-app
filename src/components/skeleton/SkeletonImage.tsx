7e5b687167b658ce1bfaff37d46100ce49ee779e
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2023-11-11 12:17:42 +1100

    adjust table skeleton

--- src/components/skeleton/SkeletonImage.tsx
+++ src/components/skeleton/SkeletonImage.tsx
@@ -6,7 +6,10 @@ type TProps = TRestProps & { className?: string }
 export const SkeletonImage = ({ className, ...rest }: TProps) => {
   return (
     <div
-      className={twMerge(className, 'flex animate-pulse items-center justify-center rounded-md border-2 bg-gray-300')}
+      className={twMerge(
+        className,
+        'flex animate-pulse items-center justify-center rounded-md border-2 dark:bg-neutral-300 bg-neutral-400',
+      )}
       {...rest}
     >
       <ImageIcon className='h-10 w-10' fill='rgb(107 114 128)' />
