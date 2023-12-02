fe50bf00147b61476a8e8fcf377bab7998c637c0
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2023-12-02 14:44:23 +1100

    fix bad useEffect implmentation in ScreenForm

--- src/components/screen/create-edit/drawer/ScreenFormDrawer.tsx
+++ src/components/screen/create-edit/drawer/ScreenFormDrawer.tsx
@@ -1,28 +1,18 @@
 import { keepPreviousData } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
-import { FormDrawerMode } from '../../../../contexts/FormDrawer/FormDrawerManager'
+import { FormDrawerActionTypes, FormDrawerMode } from '../../../../contexts/FormDrawer/FormDrawerManager'
 import { useFormDrawerContext } from '../../../../contexts/FormDrawer/useFormDrawaerContext'
 import { ScreenInput } from '../../../../generated/openapi/models'
 import { FindScreenOptions, useFindScreen } from '../../../../hooks/api/useFindScreens'
 import { transformScreenItem } from '../../../../utils/ScreenTransformation'
-import { DefaultValues } from '../form/DefaultValues'
 import { ScreenForm } from '../form/ScreenForm'
 
 type Props = TReactChildren
 
 export const ScreenFormDrawer = ({ children }: Props) => {
-  const { formDrawerState } = useFormDrawerContext()
-  const [defaultValues, setDefaultValues] = useState<ScreenInput>(DefaultValues())
+  const { formDrawerState, dispatchFormDrawer } = useFormDrawerContext()
+  const [defaultValues, setDefaultValues] = useState<ScreenInput>()
   const [editMode, setEditMode] = useState<boolean>(false)
-
-  useEffect(() => {
-    if (formDrawerState.mode === FormDrawerMode.Create || formDrawerState.mode === FormDrawerMode.Close) {
-      setDefaultValues(DefaultValues())
-      setEditMode(false)
-    } else if (formDrawerState.mode === FormDrawerMode.Edit) {
-      setEditMode(true)
-    }
-  }, [formDrawerState.mode])
 
   const queryOptions: FindScreenOptions = {
     queryKey: ['ScreenFormDrawer', 'useFindScreen'],
@@ -30,6 +20,17 @@ export const ScreenFormDrawer = ({ children }: Props) => {
     placeholderData: keepPreviousData,
   }
   const { screenItemResponse, isScreenItemLoading } = useFindScreen(formDrawerState.id ?? '', queryOptions)
+  const onCloseAction = () => {
+    dispatchFormDrawer({ type: FormDrawerActionTypes.Toggle, payload: { open: false } })
+  }
+
+  useEffect(() => {
+    if (formDrawerState.mode === FormDrawerMode.Create || formDrawerState.mode === FormDrawerMode.Close) {
+      setEditMode(false)
+    } else if (formDrawerState.mode === FormDrawerMode.Edit) {
+      setEditMode(true)
+    }
+  }, [formDrawerState.mode])
 
   useEffect(() => {
     if (screenItemResponse && formDrawerState.mode === FormDrawerMode.Edit && !isScreenItemLoading) {
@@ -53,7 +54,12 @@ export const ScreenFormDrawer = ({ children }: Props) => {
       <div className='drawer-side absolute h-fit w-auto duration-100 ease-in'>
         <div className='sidebar flex-1 rounded-xl p-2 md:w-96'>
           <div className='p-2'>
-            <ScreenForm defaultValues={defaultValues} editMode={editMode} isLoading={editMode && isScreenItemLoading} />
+            <ScreenForm
+              defaultValues={defaultValues}
+              editId={formDrawerState.id}
+              isLoading={editMode && isScreenItemLoading}
+              onCloseAction={onCloseAction}
+            />
           </div>
         </div>
       </div>
