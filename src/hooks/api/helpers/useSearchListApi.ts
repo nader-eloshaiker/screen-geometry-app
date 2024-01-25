619dbc503340f78c775dad731d2186d335086c65
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2024-01-25 21:07:18 +1100

    fix(test): revert changes to useSearchListApi

--- src/hooks/api/helpers/useSearchListApi.ts
+++ src/hooks/api/helpers/useSearchListApi.ts
@@ -1,34 +1,18 @@
-import { NotificationActionTypes, NotificationType } from '@contexts/Notification/NotificationManager'
-import { useNotificationContext } from '@contexts/Notification/useNotifcationContext'
-import { GetSearchListParams } from '@openapi/generated/models'
+import { GetSearchListParams, SearchListResponse } from '@openapi/generated/models'
 import { useGetSearchList } from '@openapi/generated/services/search-list-service'
 import { keepPreviousData } from '@tanstack/react-query'
-import { useEffect } from 'react'
+import { useApiQuery } from '../useApiQuery'
 
 // type ActionParams = Parameters<typeof useSearchList<SearchListResponse>, ErrorResponse>>
 // type QueryOptions = ActionParams[0]
 
 export const useSearchListApi = (params: GetSearchListParams) => {
-  const { dispatch } = useNotificationContext()
+  const useRequest = () =>
+    useGetSearchList(params, {
+      query: { queryKey: ['useGetSearchList', ...Object.values(params)], placeholderData: keepPreviousData },
+    })
 
-  const response = useGetSearchList(params, {
-    query: { queryKey: ['useGetSearchList', ...Object.values(params)], placeholderData: keepPreviousData },
+  return useApiQuery<SearchListResponse>({
+    useRequest,
   })
-
-  // Note: debugging only, remove when done
-  if (!response) {
-    console.error('request: is undefined')
-    // console.trace()
-  }
-
-  useEffect(() => {
-    if (response.error) {
-      dispatch({
-        type: NotificationActionTypes.ADD_NOTIFICATION,
-        payload: { value: response.error, type: NotificationType.ERROR },
-      })
-    }
-  }, [dispatch, response.error])
-
-  return response
 }
