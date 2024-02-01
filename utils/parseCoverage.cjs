ed0378cf7d02fbae5b61561402b45578a27a5265
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2024-02-01 12:58:01 +1100

    feat(test): tweak messages in badges

--- utils/parseCoverage.cjs
+++ utils/parseCoverage.cjs
@@ -1,6 +1,42 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const jsonfile = require('jsonfile')
 const { mkdirp } = require('mkdirp')
+const https = require('https')
+const fs = require('fs')
+
+const getMessageColor = (percentage) => {
+  let color = 'brightgreen'
+  if (percentage < 50) {
+    color = 'red'
+  } else if (percentage < 65) {
+    color = 'yellow'
+  } else if (percentage < 80) {
+    color = 'orange'
+  }
+  return color
+}
+
+const getBadgeObject = (label, message) => ({
+  label,
+  message: String(message) + '%',
+  namedLogo: 'vitest',
+  color: getMessageColor(message),
+  labelColor: '#444d57',
+})
+
+const downloadBadge = (url, filename) =>
+  https.get(url, (res) => {
+    if (res.statusCode !== 200) {
+      console.error(`Error downloading badge: ${res.statusMessage}`)
+      return
+    }
+
+    const file = fs.createWriteStream(filename)
+    res.pipe(file)
+    file.on('finish', () => {
+      file.close()
+    })
+  })
 
 jsonfile
   .readFile('./reports/coverage/coverage-summary.json')
@@ -19,35 +55,22 @@ jsonfile
       .then(() => Promise.resolve(data))
       .catch((err) => Promise.reject(err)),
   )
-  .then(({ Coverage, Lines, Statements, Functions, Branches }) =>
-    Promise.all([
-      jsonfile.writeFile(
-        './reports/badges/coverage.json',
-        { label: 'Coverage', message: String(Coverage), namedLogo: 'vitest' },
-        { spaces: 2 },
-      ),
-      jsonfile.writeFile(
-        './reports/badges/lines.json',
-        { label: 'Lines', message: String(Lines), namedLogo: 'vitest' },
-        { spaces: 2 },
-      ),
-      jsonfile.writeFile(
-        './reports/badges/statements.json',
-        { label: 'Statements', message: String(Statements), namedLogo: 'vitest' },
-        { spaces: 2 },
-      ),
-      jsonfile.writeFile(
-        './reports/badges/functions.json',
-        { label: 'Functions', message: String(Functions), namedLogo: 'vitest' },
-        { spaces: 2 },
-      ),
-      jsonfile.writeFile(
-        './reports/badges/branches.json',
-        { label: 'Branches', message: String(Branches), namedLogo: 'vitest' },
-        { spaces: 2 },
-      ),
-    ]),
-  )
+  .then(({ Coverage, Lines, Statements, Functions, Branches }) => {
+    const coverage = getBadgeObject('Coverage', Coverage)
+    const lines = getBadgeObject('Lines', Lines)
+    const statements = getBadgeObject('Statements', Statements)
+    const functions = getBadgeObject('Functions', Functions)
+    const branches = getBadgeObject('Branches', Branches)
+
+    return Promise.all([
+      Promise.resolve({ coverage, lines, statements, functions, branches }),
+      jsonfile.writeFile('./reports/badges/coverage.json', coverage, { spaces: 2 }),
+      jsonfile.writeFile('./reports/badges/lines.json', lines, { spaces: 2 }),
+      jsonfile.writeFile('./reports/badges/statements.json', statements, { spaces: 2 }),
+      jsonfile.writeFile('./reports/badges/functions.json', functions, { spaces: 2 }),
+      jsonfile.writeFile('./reports/badges/branches.json', branches, { spaces: 2 }),
+    ])
+  })
   .catch((err) => {
     console.error(err)
   })
