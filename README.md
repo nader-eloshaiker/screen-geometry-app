7a24ccb00f63ba3fb7f2cadbabf440214e911a88
Nader Eloshaiker <1904325+nader-eloshaiker@users.noreply.github.com>
2024-02-01 14:28:45 +1100

    feat(test): adjust coverage badges (#290)
    * feat(test): generate badge data
    
    * chore(ops): temporarily modify ci/cd to deploy reports from validation job
    
    * fix(test): reference to parseCoverage.cjs
    
    * fix(test): use correct shields.io api
    
    * fix(test): enforce string values in message
    
    * feat(test): tweak messages in badges
    
    * feat(test): add all coverage data using local asset
    
    * feat(test): revert validation pipeline now that testing is finished
    
    * feat(test): remove decimals from coverage
    
    * feat(test): adjust name tag
    
    * feat(test): rename utility

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 [![CI/CD](https://github.com/nader-eloshaiker/screen-geometry-app/actions/workflows/cicd.yml/badge.svg)](https://github.com/nader-eloshaiker/screen-geometry-app/actions/workflows/cicd.yml)
 [![Release Deployment](https://github.com/nader-eloshaiker/screen-geometry-app/actions/workflows/release.yml/badge.svg)](https://github.com/nader-eloshaiker/screen-geometry-app/actions/workflows/release.yml)
 
-![Coverage Badge](https://nader-eloshaiker.github.io/screen-geometry-app/badges/statements.svg) ![Coverage Badge](https://nader-eloshaiker.github.io/screen-geometry-app/badges/branches.svg) ![Coverage Badge](https://nader-eloshaiker.github.io/screen-geometry-app/badges/functions.svg)
-![Coverage Badge](https://nader-eloshaiker.github.io/screen-geometry-app/badges/lines.svg)
+![Statements Badge](https://nader-eloshaiker.github.io/screen-geometry-app/badges/statements.svg) ![Branches Badge](https://nader-eloshaiker.github.io/screen-geometry-app/badges/branches.svg) ![Functions Badge](https://nader-eloshaiker.github.io/screen-geometry-app/badges/functions.svg)
+![Lines Badge](https://nader-eloshaiker.github.io/screen-geometry-app/badges/lines.svg)
 
 ## Description
 
---
+++ utils/createCoverageBadges.cjs
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const jsonfile = require('jsonfile')
+const { mkdirp } = require('mkdirp')
+const https = require('https')
+const fs = require('fs')
+
+const getMessageColor = (percentage) => {
+  let color = 'brightgreen'
+  if (percentage < 50) {
+    color = 'red'
+  } else if (percentage < 65) {
+    color = 'yellow'
+  } else if (percentage < 80) {
+    color = 'orange'
+  }
+  return color
+}
+
+const getBadgeObject = (label, message) => ({
+  label,
+  message: String(message.toFixed()) + '%',
+  namedLogo: 'vitest',
+  color: getMessageColor(message),
+  labelColor: '#444d57',
+})
+
+const getBadgeUrl = (badgeObj) =>
+  `https://img.shields.io/badge/${encodeURI(badgeObj.label)}-${encodeURI(badgeObj.message)}-${encodeURI(
+    badgeObj.color,
+  )}.svg?logo=${encodeURI(badgeObj.namedLogo)}`
+
+const downloadBadge = (url, filename) =>
+  https.get(url, (res) => {
+    if (res.statusCode !== 200) {
+      console.error(`Error downloading badge: ${res.statusMessage}`)
+      return
+    }
+
+    const file = fs.createWriteStream(filename)
+    res.pipe(file)
+    file.on('finish', () => {
+      file.close()
+    })
+  })
+
+jsonfile
+  .readFile('./reports/coverage/coverage-summary.json')
+  .then((summary) => {
+    const { total } = summary
+    const { lines, statements, functions, branches } = total
+    const Lines = lines.pct
+    const Statements = statements.pct
+    const Functions = functions.pct
+    const Branches = branches.pct
+
+    return Promise.resolve({ Coverage: Lines, Lines, Statements, Functions, Branches })
+  })
+  .then((data) =>
+    mkdirp('./reports/badges')
+      .then(() => Promise.resolve(data))
+      .catch((err) => Promise.reject(err)),
+  )
+  .then(({ Coverage, Lines, Statements, Functions, Branches }) => {
+    const coverage = getBadgeObject('Coverage', Coverage)
+    const lines = getBadgeObject('Lines', Lines)
+    const statements = getBadgeObject('Statements', Statements)
+    const functions = getBadgeObject('Functions', Functions)
+    const branches = getBadgeObject('Branches', Branches)
+
+    return Promise.all([
+      Promise.resolve({ coverage, lines, statements, functions, branches }),
+      jsonfile.writeFile('./reports/badges/coverage.json', coverage, { spaces: 2 }),
+      jsonfile.writeFile('./reports/badges/lines.json', lines, { spaces: 2 }),
+      jsonfile.writeFile('./reports/badges/statements.json', statements, { spaces: 2 }),
+      jsonfile.writeFile('./reports/badges/functions.json', functions, { spaces: 2 }),
+      jsonfile.writeFile('./reports/badges/branches.json', branches, { spaces: 2 }),
+    ])
+  })
+  .then(([{ coverage, lines, statements, functions, branches }]) => {
+    downloadBadge(getBadgeUrl(coverage), './reports/badges/coverage.svg')
+    downloadBadge(getBadgeUrl(lines), './reports/badges/lines.svg')
+    downloadBadge(getBadgeUrl(statements), './reports/badges/statements.svg')
+    downloadBadge(getBadgeUrl(functions), './reports/badges/functions.svg')
+    downloadBadge(getBadgeUrl(branches), './reports/badges/branches.svg')
+  })
+  .catch((err) => {
+    console.error(err)
+  })
