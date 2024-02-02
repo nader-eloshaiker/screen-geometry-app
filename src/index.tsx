8a516dd647e03ce54e4dc43a634042b1aefef416
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2024-02-02 23:23:39 +1100

    fix(app): bad refernce to placeholdersrc and remove setupServer from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { generateStub } from '@server/server'
 import { setupWorker } from 'msw/browser'
-import { setupServer } from 'msw/node'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
@@ -10,12 +9,11 @@ const createServiceWorker = async () => {
   const { searchMocks, screenListMocks, screenMocks, passthroughMocks } = generateStub()
 
   if (import.meta.env.NODE_ENV) {
-    const nodeServer = setupServer(...searchMocks(), ...screenListMocks(), ...screenMocks(), ...passthroughMocks())
-    return await nodeServer.listen()
-  } else {
-    const webServer = setupWorker(...searchMocks(), ...screenListMocks(), ...screenMocks(), ...passthroughMocks())
-    return await webServer.start()
+    return Promise.resolve()
   }
+
+  const webServer = setupWorker(...searchMocks(), ...screenListMocks(), ...screenMocks(), ...passthroughMocks())
+  return await webServer.start()
 }
 
 createServiceWorker().then(() => {
