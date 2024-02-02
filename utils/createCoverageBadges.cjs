97acb0ab581c5eadcb390a2f38876740cd29c8c9
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2024-02-02 23:42:27 +1100

    fix(docs): move badges to docs to avoid caching

--- utils/createCoverageBadges.cjs
+++ utils/createCoverageBadges.cjs
@@ -3,6 +3,7 @@ const jsonfile = require('jsonfile')
 const { mkdirp } = require('mkdirp')
 const https = require('https')
 const fs = require('fs')
+const outputPath = './docs/badges'
 
 const getMessageColor = (percentage) => {
   let color = 'brightgreen'
@@ -56,7 +57,7 @@ jsonfile
     return Promise.resolve({ Coverage: Lines, Lines, Statements, Functions, Branches })
   })
   .then((data) =>
-    mkdirp('./reports/badges')
+    mkdirp(outputPath)
       .then(() => Promise.resolve(data))
       .catch((err) => Promise.reject(err)),
   )
@@ -69,19 +70,19 @@ jsonfile
 
     return Promise.all([
       Promise.resolve({ coverage, lines, statements, functions, branches }),
-      jsonfile.writeFile('./reports/badges/coverage.json', coverage, { spaces: 2 }),
-      jsonfile.writeFile('./reports/badges/lines.json', lines, { spaces: 2 }),
-      jsonfile.writeFile('./reports/badges/statements.json', statements, { spaces: 2 }),
-      jsonfile.writeFile('./reports/badges/functions.json', functions, { spaces: 2 }),
-      jsonfile.writeFile('./reports/badges/branches.json', branches, { spaces: 2 }),
+      jsonfile.writeFile(outputPath + '/coverage.json', coverage, { spaces: 2 }),
+      jsonfile.writeFile(outputPath + '/lines.json', lines, { spaces: 2 }),
+      jsonfile.writeFile(outputPath + '/statements.json', statements, { spaces: 2 }),
+      jsonfile.writeFile(outputPath + '/functions.json', functions, { spaces: 2 }),
+      jsonfile.writeFile(outputPath + '/branches.json', branches, { spaces: 2 }),
     ])
   })
   .then(([{ coverage, lines, statements, functions, branches }]) => {
-    downloadBadge(getBadgeUrl(coverage), './reports/badges/coverage.svg')
-    downloadBadge(getBadgeUrl(lines), './reports/badges/lines.svg')
-    downloadBadge(getBadgeUrl(statements), './reports/badges/statements.svg')
-    downloadBadge(getBadgeUrl(functions), './reports/badges/functions.svg')
-    downloadBadge(getBadgeUrl(branches), './reports/badges/branches.svg')
+    downloadBadge(getBadgeUrl(coverage), outputPath + '/coverage.svg')
+    downloadBadge(getBadgeUrl(lines), outputPath + '/lines.svg')
+    downloadBadge(getBadgeUrl(statements), outputPath + '/statements.svg')
+    downloadBadge(getBadgeUrl(functions), outputPath + '/functions.svg')
+    downloadBadge(getBadgeUrl(branches), outputPath + '/branches.svg')
   })
   .catch((err) => {
     console.error(err)
