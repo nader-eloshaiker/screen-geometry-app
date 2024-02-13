6a1357750a16cdc83f0f710006c3d17e95d34202
Nader Eloshaiker <1904325+nader-eloshaiker@users.noreply.github.com>
2024-02-13 21:44:49 +1100

    feat: extend colors to overscroll (#338)
    feat(app): extend colors to overscroll

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
