7fbe1bf5659968f47a26ce193795e44f715743fd
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2024-02-28 19:18:16 +1100

    feat(app): disable refetch on window blur

--- src/test/utils/QueryTestOptions.ts
+++
@@ -1,9 +0,0 @@
-import { QueryClientConfig } from '@tanstack/react-query'
-
-export const QueryTestOptions: QueryClientConfig = {
-  defaultOptions: {
-    queries: {
-      retry: false, // turn off retries for testing
-    },
-  },
-}
