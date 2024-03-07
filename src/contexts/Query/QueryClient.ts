ee05f442141896b63c36602cecb33dba13aebb6d
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2024-03-07 11:07:37 +1100

    fix(app): prevent refetch on window blur with react-query and notification placement

--- src/contexts/Query/QueryClient.ts
+++ src/contexts/Query/QueryClient.ts
@@ -1,5 +1,14 @@
 import { QueryClient } from '@tanstack/react-query'
-import { QueryTestOptions } from '@test/utils/QueryTestOptions'
+import axios from 'axios'
 
-const finalOptions = import.meta.env.NODE_ENV ? QueryTestOptions : {}
+const finalOptions = {
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: !import.meta.env.NODE_ENV, // turn off retries for unit testing
+    },
+  },
+}
 export const queryClient = new QueryClient(finalOptions)
+export const baseUrl = import.meta.env.VITE_API_URL ?? 'http://dev.api.screengeometry.com'
+axios.defaults.baseURL = baseUrl
