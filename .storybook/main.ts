aef4d7dc9bd1e866be7bfc31edd676f04f15ba99
Nader Eloshaiker <1904325+nader-eloshaiker@users.noreply.github.com>
2024-04-08 20:47:38 +1000

    feat(test): storybook testing (#400)
    * feat(test): storybook testing
    
    * feat(test): remove redundant build step in actions
    
    * feat(test): add playwright install for storybook testing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-themes',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/react-vite',
