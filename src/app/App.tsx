8441162c363548b737f2178a9e146a11a1893e8b
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2024-09-15 21:32:19 +1000

    feat!(db): change initDB to run in server init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,27 +2,17 @@ import { QueryProvider } from '@app/contexts/Query/QueryProvider'
 import { ScreenProvider } from '@app/contexts/Screen/ScreenProvider'
 import { ThemeModeProvider } from '@app/contexts/theme/ThemeModeProvider'
 import AppRouterProvider from '@app/routes/AppRouterProvider'
-import { initDB } from '@packages/server/db/IndexedDB'
 import { NotificationProvider } from '@packages/ui/notification'
-import { useEffect } from 'react'
 import '../index.css'
 
-export const App = () => {
-  useEffect(() => {
-    const connectToDB = async () => await initDB()
-
-    connectToDB().catch(console.error)
-  }, [])
-
-  return (
-    <QueryProvider>
-      <ThemeModeProvider>
-        <NotificationProvider>
-          <ScreenProvider>
-            <AppRouterProvider />
-          </ScreenProvider>
-        </NotificationProvider>
-      </ThemeModeProvider>
-    </QueryProvider>
-  )
-}
+export const App = () => (
+  <QueryProvider>
+    <ThemeModeProvider>
+      <NotificationProvider>
+        <ScreenProvider>
+          <AppRouterProvider />
+        </ScreenProvider>
+      </NotificationProvider>
+    </ThemeModeProvider>
+  </QueryProvider>
+)
