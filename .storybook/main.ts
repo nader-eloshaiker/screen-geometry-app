0df23d4c7c025cf0ca82894c0eaccad1aa6c6c81
Nader Eloshaiker <nader.eloshaiker@pepperstone.com>
2025-03-07 08:56:21 +1100

    fix sb with new vite ver

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -21,9 +21,7 @@ const coverageConfig: AddonOptionsVite = {
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
     '@storybook/addon-themes',
     'storybook-addon-pseudo-states',
