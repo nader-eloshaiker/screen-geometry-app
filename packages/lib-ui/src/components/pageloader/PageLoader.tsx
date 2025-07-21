5c5f7cb6c8a692950be26c8a7c1a97ee97641c83
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2025-07-22 09:39:45 +1000

    fix tests

--- packages/lib-ui/src/components/pageloader/PageLoader.tsx
+++ packages/lib-ui/src/components/pageloader/PageLoader.tsx
@@ -7,25 +7,24 @@ interface Props {
 
 export const PageLoader = ({ message }: Props) => {
   return (
-    <div>
-      <div className='flex h-full flex-col justify-between bg-white md:mx-auto md:justify-normal md:rounded-3xl lg:w-[780px] lg:bg-transparent'>
-        <div className='flex h-screen flex-col items-center justify-center'>
-          <>
-            <div
-              className='relative z-50 flex items-center justify-center py-24'
-              data-testid='loaderWithIcon'
-              role='status'
-            >
-              <div className='absolute size-[250px] rounded-full'>
-                <div className='absolute inset-0 flex items-center justify-center'>
-                  <SpinnerIcon />
-                </div>
+    // <div>
+    <div
+      data-testid='page-loader'
+      className='flex h-full flex-col justify-between bg-white md:mx-auto md:justify-normal md:rounded-3xl lg:w-[780px] lg:bg-transparent'
+    >
+      <div className='flex h-screen flex-col items-center justify-center'>
+        <>
+          <div className='relative z-50 flex items-center justify-center py-24' role='status'>
+            <div className='absolute size-[250px] rounded-full'>
+              <div className='absolute inset-0 flex items-center justify-center'>
+                <SpinnerIcon />
               </div>
             </div>
-            {message && <div className='mt-[-70px] text-xl font-bold'>{message}</div>}
-          </>
-        </div>
+          </div>
+          {message && <div className='mt-[-70px] text-xl font-bold'>{message}</div>}
+        </>
       </div>
     </div>
+    // </div>
   )
 }
