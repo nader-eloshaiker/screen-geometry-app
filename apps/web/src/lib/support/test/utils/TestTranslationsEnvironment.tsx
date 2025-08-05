241596776813c0bb982e1f7c354563bef20fa786
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2025-08-05 21:42:24 +1000

    simplify translation mock

--- apps/web/src/lib/support/test/utils/TestTranslationsEnvironment.tsx
+++ apps/web/src/lib/support/test/utils/TestTranslationsEnvironment.tsx
@@ -1,12 +1,11 @@
 import { IntlProvider } from 'react-intl'
-import messages from '../../../../../public/locales/en-US.json'
 
 // vi.mock('react-intl', () => ({
 //   IntlProvider: vi.fn(({ children }) => <div data-testid='Intl-provider'>{children}</div>),
 // }))
 
 export const TestTranslationsEnvironment = ({ children }: { children: React.ReactNode }) => (
-  <IntlProvider locale='en' messages={messages} defaultLocale='en'>
+  <IntlProvider locale='en' messages={{}} defaultLocale='en'>
     {children}
   </IntlProvider>
 )
