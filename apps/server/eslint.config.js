18afaf270f18347a84b46f14567c5fc892da6f7e
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2025-08-14 09:48:31 +1000

    uplift eslint

--- apps/server/eslint.config.js
+++ apps/server/eslint.config.js
@@ -1,97 +1,84 @@
 // import { FlatCompat } from '@eslint/eslintrc'
-import jsEslint from '@eslint/js'
-import typescriptParser from '@typescript-eslint/parser'
-import eslintConfigPrettier from 'eslint-config-prettier/flat'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslint from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import configPrettier from 'eslint-config-prettier/flat'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier/recommended'
 import react from 'eslint-plugin-react'
-import { defineConfig } from 'eslint/config'
-import globals from 'globals/index.js'
-import tsEslint from 'typescript-eslint'
-
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
-export default defineConfig([
-  jsEslint.configs.recommended,
-  ...tsEslint.configs.recommended,
-  eslintPluginPrettierRecommended,
+export default tseslint.config(
+  globalIgnores(['dist/**/*'], 'Ignore Build Directory'),
+  eslint.configs.recommended,
+  pluginQuery.configs['flat/recommended'],
   reactHooks.configs['recommended-latest'],
-  reactRefresh.configs.recommended,
+  reactRefresh.configs.vite,
+  tseslint.configs.recommended,
   {
     ignores: ['node_modules/', 'dist/', 'coverage/'],
   },
   {
     files: ['**/*.{js,mjs,ts,tsx,jsx}'],
 
-    plugins: {
-      react,
-      '@typescript-eslint': tsEslint.plugin,
-    },
-
     languageOptions: {
+      ecmaVersion: 'latest',
       globals: {
         ...globals.browser,
         ...globals.vitest,
         ...globals.node,
-        window: true,
         module: true,
+        window: true,
       },
-
-      parser: typescriptParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
 
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
         },
       },
+      sourceType: 'module',
     },
 
-    settings: {
-      react: {
-        createClass: 'createReactClass',
-        pragma: 'React',
-        fragment: 'Fragment',
-        version: 'detect',
-        flowVersion: '0.53',
-      },
+    plugins: {
+      importPlugin,
+      react,
     },
 
     rules: {
       '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
 
-      'no-unused-vars': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
-          vars: 'all', // Check all variables for usage
-          ignoreRestSiblings: true,
           argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
           caughtErrorsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+          vars: 'all', // Check all variables for usage
+          varsIgnorePattern: '^_',
         },
       ],
+      camelcase: 'error',
+      'no-duplicate-imports': 'error',
 
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
+      'no-empty-function': 'off',
+
+      'no-unused-vars': 'off',
 
       'prettier/prettier': [
         'error',
         {
-          semi: false,
           endOfLine: 'auto',
+          semi: false,
         },
       ],
-
-      'no-empty-function': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react/prop-types': 'off',
-      camelcase: 'error',
       quotes: ['error', 'single'],
-      'no-duplicate-imports': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
 
       'react-refresh/only-export-components': [
         'warn',
@@ -99,7 +86,24 @@ export default defineConfig([
           allowConstantExport: true,
         },
       ],
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'sort-keys-custom-order/import-object-keys': 'off', // handled by prettier
+    },
+
+    settings: {
+      react: {
+        createClass: 'createReactClass',
+        flowVersion: '0.53',
+        fragment: 'Fragment',
+        pragma: 'React',
+        version: 'detect',
+      },
+      tailwindcss: {
+        config: './tailwind.config.ts',
+      },
     },
   },
-  eslintConfigPrettier, // Must go last
-])
+  configPrettier, // Must go second last
+  prettierPlugin // Must go last
+)
