2a949f09b790590a72b2af0294c57a88b5f1c08f
Nader Eloshaiker <1904325+nader-eloshaiker@users.noreply.github.com>
2025-11-17 20:39:56 +1100

    fix(app): code coverage exclusions and validation badge (#630)
    * fix(app): code coverage exclusions and validation badge
    
    * fix(ops): remove deploy branches

--- packages/lib-api/vite.config.ts
+++ packages/lib-api/vite.config.ts
@@ -20,6 +20,7 @@ const Config: ViteUserConfig = {
         ...configDefaults.exclude,
         'src/generated',
         'src/extended/models',
+        'src/ggl',
         'src/configs',
         'src/lib',
         'src/**/*.mock.{ts,tsx}',
