92cd7242d66de2bb3f0b574c9578f2d1d5090f5e
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2026-04-25 22:35:33 +1000

    fix(build): package-lock

--- packages/lib-ui/src/components/navigationmenu/NavigationMenu.tsx
+++ packages/lib-ui/src/components/navigationmenu/NavigationMenu.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { cva } from 'class-variance-authority'
 import { ChevronDownIcon } from 'lucide-react'
 import { NavigationMenu as NavigationMenuPrimitive } from 'radix-ui'
